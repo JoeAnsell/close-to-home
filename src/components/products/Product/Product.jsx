386acf5a77de5a527bd466d2cac6a43bf669d2b5
Joseph Ansell <anselljoseph19@gmail.com>
2021-05-08 20:55:37 +0100

    created Product structure

---
+++ src/components/products/Product/Product.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+import classes from "*.module.css";
+
+const Product = ({ product }) => {
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image=""
+        title={product.name}
+      ></CardMedia>
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            {product.price}
+          </Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary" gutterBottom>
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
