11c106b7f6476a28071c6de8ec3b3ce8e4d679e6
Joseph Ansell <anselljoseph19@gmail.com>
2021-05-09 20:32:15 +0100

    added own styles to nav bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 // import NavBar from "./components/NavBar/Navbar";
 
 import { Products, Navbar } from "./components";
+import { createGlobalStyle } from "styled-components";
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <Navbar />
       <Products />
     </div>
@@ -14,3 +16,36 @@ const App = () => {
 };
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: "futura", "arial", san-serif;
+    line-height: 1.3;
+    font-size: 20px;
+    
+    body{
+      background-color: white;
+    }
+
+    a,a:visited{
+      color: inherit;
+    }
+    button{
+      -webkit-tap-highlight-color: transparent;
+    }
+
+
+
+    h1{
+      font-size: 1.7rem;
+      line-height: 1.8rem;
+    }
+    h2{
+      font-size: 1.2rem;
+      line-height: 1.3rem;
+    }
+  }
+`;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  AppBar,
   Toolbar,
   IconButton,
   Badge,
@@ -9,14 +8,17 @@ import {
   Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import styled from "styled-components";
 // import classes from "*.module.css";
 
 const Navbar = () => {
   return (
     <>
       <AppBar position="fixed" color="inherit">
-        <Toolbar>
+        <Left>
           <h4>Close to Home</h4>
+        </Left>
+        <Right>
           <div>
             <IconButton arial-label="Show cart items" color="inherit">
               <Badge badgecontent={2} color="secondary">
@@ -24,10 +26,30 @@ const Navbar = () => {
               </Badge>
             </IconButton>
           </div>
-        </Toolbar>
+        </Right>
       </AppBar>
     </>
   );
 };
 
 export default Navbar;
+
+const AppBar = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  max-width: 100vw;
+  justify-content: space-between;
+  border: 2px solid red;
+  background-color: white;
+  width: 100%;
+`;
+
+const Left = styled.div`
+  ${"" /* width: 200px; */}
+  margin: 20px;
+`;
+const Right = styled.div`
+  ${"" /* width: 200px; */}
+  margin: 20px;
+`;
