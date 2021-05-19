8fc19c5b96e0b14d6d29cd11e5ac32422d797c4d
Joseph Ansell <anselljoseph19@gmail.com>
2021-05-19 23:05:14 +0100

    re-added keys

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce(
-  "pk_test_27186b92b3a7a7e9e438a5a33f6887335c734827e6ae4"
-);
+export const commerce = new Commerce(process.env.REACT_APP_CHECK_PUBLIC_KEY);
