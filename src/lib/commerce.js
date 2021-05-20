0335c9c907d7a55b9d5a8b402794eeae26d194df
Joseph Ansell <anselljoseph19@gmail.com>
2021-05-20 20:04:16 +0100

    updated api key

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,4 @@
 import Commerce from "@chec/commerce.js";
-import { Link, useLocation } from "react-router-dom";
 
 export const commerce = new Commerce(
   `${process.env.REACT_APP_CHECK_PUBLIC_KEY}`,
